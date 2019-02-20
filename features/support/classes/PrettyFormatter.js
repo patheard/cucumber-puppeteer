0052de612ebd06fc78aac550df589b571c634352
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-02-20 11:54:19 -0500

    Add custom PrettyFormatter and removed cucumber-pretty dep

---
+++ features/support/classes/PrettyFormatter.js
@@ -0,0 +1,43 @@
+const {Status, SummaryFormatter } = require('cucumber');
+const STATUS_CHARACTER_MAPPING = {
+  [Status.AMBIGUOUS]: '~',
+  [Status.FAILED]: '×',
+  [Status.PASSED]: '√',
+  [Status.PENDING]: '/',
+  [Status.SKIPPED]: '-',
+  [Status.UNDEFINED]: '?',
+}
+
+/**
+ * Provide pretty formatting of test results
+ */
+class PrettyFormatter extends SummaryFormatter {
+  constructor(options) {
+    super(options)
+    options.eventBroadcaster
+      .on('test-case-started', this.logTestCaseName.bind(this))
+      .on('test-step-finished', this.logTestStep.bind(this))
+      .on('test-case-finished', this.logSeparator.bind(this))
+
+    this.scenarioCount = 1;
+  }
+
+  logTestCaseName({sourceLocation}) {
+    const {pickle} = this.eventDataCollector.getTestCaseData(sourceLocation)
+    this.log(this.scenarioCount++ + ') Scenario: ' + pickle.name + ' ' + this.colorFns['location'](sourceLocation.uri) + '\n');
+  }
+
+  logTestStep({testCase, index, result}) {
+    const {gherkinKeyword, pickleStep} = this.eventDataCollector.getTestStepData({testCase, index});
+    const {status} = result;
+    if (pickleStep) {
+      this.log(this.colorFns[status](STATUS_CHARACTER_MAPPING[status]) + ' ' + gherkinKeyword + pickleStep.text + '\n');
+    }
+  }
+
+  logSeparator() {
+    this.log('\n');
+  }
+}
+
+module.exports = PrettyFormatter;
