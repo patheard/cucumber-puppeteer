853629d0ca3dfa87cb3aa49c94e54b5538deb174
Pat Heard <pat.heard@gmail.com>
2019-02-23 11:25:25 -0500

    Move config.js

--- features/support/config.js
+++ features/support/config.js
@@ -3,13 +3,13 @@
  * https://github.com/cucumber/cucumber-js/blob/master/docs/support_files/api_reference.md
  */
 const { After, AfterAll, Before, setDefaultTimeout, setWorldConstructor } = require('cucumber');
-const FeatureScope = require('./support/scope/FeatureScope');
-const BrowserScope = require('./support/scope/BrowserScope');
+const FeatureScope = require('./scope/FeatureScope');
+const BrowserScope = require('./scope/BrowserScope');
 
 // Timeout, in milliseconds, for puppeteer actions
 setDefaultTimeout(10 * 1000);
 
-// `ScenarioScope` is provided to all hooks and test steps in a scenario as `this`
+// `BrowserScope` is provided to all hooks and test steps in a scenario as `this`
 setWorldConstructor(BrowserScope)
 
 // Keep a consistent web browser and page for all scenarios in a feature file.
