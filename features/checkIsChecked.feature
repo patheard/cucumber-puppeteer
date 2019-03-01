08ae547f281e966c984d221bc6a2bc96a5657942
valentinmagot <valentin_magot@outlook.com>
2019-03-01 18:38:05 -0500

    Added checkIsChecked steps and tests.

---
+++ features/checkIsChecked.feature
@@ -0,0 +1,14 @@
+Feature: checkIsChecked
+  In order write BDD tests
+  As a developer
+  I want to check if an element is checked or unchecked
+
+  Scenario: Check element is checked
+    When  I open the url "http://localhost:8080/checkIsChecked.html"
+    Then  I expect the element "#checked" is checked
+    And   I expect the element "#not-checked" is not checked
+
+  Scenario: Check element exists given
+    Given the element "#checked" is checked
+    Then  the element "#not-checked" is not checked
+
---
+++ test/check/checkIsChecked.test.js
@@ -0,0 +1,39 @@
+const checkIsChecked= require('../../features/support/check/checkIsChecked');
+const openUrl = require('../../features/support/action/openUrl');
+const BrowserScope = require('../../features/support/scope/BrowserScope');
+
+const testTimeout = 10000;
+const testUrl = 'http://localhost:8080/checkIsChecked.html';
+const browserScope = new BrowserScope();
+
+beforeAll(async () => {
+  await browserScope.init();
+  await openUrl.call(browserScope, testUrl);
+});
+afterAll(async () => {
+  await browserScope.close();
+});
+
+describe('checkIsChecked', () => {
+
+    it("finds element that is checked", async () =>{
+        await checkIsChecked.call(browserScope, '#checked');
+    }, testTimeout);
+    
+    it("finds element that is unchecked", async () =>{
+        await checkIsChecked.call(browserScope, '#not-checked', 'not');
+    }, testTimeout);
+
+    it("throws an error when the element is uncheked, but should be checked", async () =>{
+        await expect(checkIsChecked.call(browserScope, '#not-checked')).rejects.toThrow('Expected "#not-checked" to be checked'); 
+    }, testTimeout);
+
+    it("throws an error when the element is cheked, but should be unchecked", async () =>{
+        await expect(checkIsChecked.call(browserScope, '#checked', 'not')).rejects.toThrow('Expected "#checked" to be unchecked'); 
+    }, testTimeout);
+
+    it('throws an error when the element does not exist', async () => {
+        await expect(checkIsChecked.call(browserScope, '.foobar')).rejects.toThrow('Error: failed to find element matching selector ".foobar"');
+    }, testTimeout); 
+    
+}); 
