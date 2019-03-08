9ca5acaa1c58c42723a8d336107e33e03805a6d3
Valentin Magot <MagotV@ad.pwgsc-tpsgc.gc.ca>
2019-03-08 15:50:06 -0500

    Added deleteCookie steps and tests

---
+++ features/deleteCookie.feature
@@ -0,0 +1,14 @@
+Feature: deleteCookie
+  In order write BDD tests
+  As a developer
+  I want to delete a cookie present in a web page
+
+  Scenario: Delete Cookie
+    When  I open the url "http://localhost:8080/checkCookieExists.html"
+    Then  I expect the cookie "existing-cookie" exist
+    And   I delete the cookie "existing-cookie"
+    And   I expect the cookie "existing-cookie" not exist
+
+  Scenario: Delete Cookie when
+    When  I delete the cookie "second-existing-cookie"
+    Then  I expect the cookie "second-existing-cookie" not exist
---
+++ test/action/deleteCookie.test.js
@@ -0,0 +1,33 @@
+const checkCookieExists = require('../../features/support/check/checkCookieExists');
+const deleteCookie = require('../../features/support/action/deleteCookie');
+const openUrl = require('../../features/support/action/openUrl');
+const BrowserScope = require('../../features/support/scope/BrowserScope');
+
+const testTimeout = 10000;
+const testUrl = 'http://localhost:8080/checkCookieExists.html';
+const browserScope = new BrowserScope();
+
+beforeAll(async () => {
+  await browserScope.init();
+  await openUrl.call(browserScope, testUrl);  
+});
+afterAll(async () => {
+  await browserScope.close();
+});
+
+describe('deleteCookie', () => {
+
+  it('delete an existing cookie', async () => {
+    await checkCookieExists.call(browserScope, 'existing-cookie');
+    await deleteCookie.call(browserScope, 'existing-cookie');
+    await checkCookieExists.call(browserScope, 'existing-cookie', 'not');
+  }, testTimeout);
+
+
+  it('fails if the cookie does not exist', async () => {
+    await checkCookieExists.call(browserScope, 'tobe-deleted-cookie', 'not');  
+    await expect(deleteCookie.call(browserScope, 'tobe-deleted-cookie')).rejects.toThrow(`Error: unable to find 'tobe-deleted-cookie'.`);
+    await expect(deleteCookie.call(browserScope)).rejects.toThrow(`Error: unable to find 'undefined'.`);
+  }, testTimeout);
+
+});
