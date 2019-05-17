7bf9ef55213905df093ddc19c2edb3af0fa7d62c
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-05-17 18:41:19 -0400

    Add waitForSelector action

---
+++ features/waitForSelector.feature
@@ -0,0 +1,17 @@
+Feature: waitForSelector
+  In order write BDD tests
+  As a developer
+  I want wait for a selector to exist on the page
+
+  Scenario: Waiting the default 30s timeout for the selector
+    Given the url "http://localhost:8080/waitForSelector.html" is opened
+    And   I expect the element ".delayed" is not on the page
+    When  I wait for element ".delayed"
+    Then  I expect the element ".delayed" is on the page
+
+  Scenario: Waiting a custom 10s timeout for the selector
+    When  I open the url "http://localhost:8080/waitForSelector.html"
+    And   I expect the element ".delayed" is not on the page
+    When  I wait for element ".delayed" for 10 seconds
+    Then  I expect the element ".delayed" is on the page
+       
---
+++ test/action/waitForSelector.test.js
@@ -0,0 +1,32 @@
+const checkElementExists = require('../../features/support/check/checkElementExists');
+const waitForSelector = require('../../features/support/action/waitForSelector');
+const openUrl = require('../../features/support/action/openUrl');
+const BrowserScope = require('../../features/support/scope/BrowserScope');
+
+const testUrl = 'http://localhost:8080/waitForSelector.html';
+const browserScope = new BrowserScope();
+
+beforeAll(async () => {  
+  await browserScope.init();  
+});
+afterAll(async () => {
+  await browserScope.close();
+});
+
+describe('waitForSelector', () => {
+
+  it('waits a default timeout for an element', async () => {
+    await openUrl.call(browserScope, testUrl);  
+    await checkElementExists.call(browserScope, '.delayed', 'not');
+    await waitForSelector.call(browserScope, '.delayed');
+    await checkElementExists.call(browserScope, '.delayed');
+  });
+
+  it('waits a specified decimal period of time for an element', async () => {
+    await openUrl.call(browserScope, testUrl);  
+    await checkElementExists.call(browserScope, '.delayed', 'not');
+    await waitForSelector.call(browserScope, '.delayed', 10);
+    await checkElementExists.call(browserScope, '.delayed');
+  });   
+
+}); 
