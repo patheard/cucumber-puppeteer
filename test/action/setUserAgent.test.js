e97127570aeabb7feb71c5d3d9c614ffc4db858e
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-05-17 12:41:30 -0400

    Made setUserAgent testing more robust

--- test/action/setUserAgent.test.js
+++ test/action/setUserAgent.test.js
@@ -1,8 +1,8 @@
 const BrowserScope = require('../../features/support/scope/BrowserScope');
 const checkContainsText = require('../../features/support/check/checkContainsText');
+const checkElementVisible = require('../../features/support/check/checkElementVisible');
 const openUrl = require('../../features/support/action/openUrl');
 const setUserAgent = require('../../features/support/action/setUserAgent');
-const waitFor = require('../../features/support/action/waitFor');
 
 const testUrl = 'http://localhost:8080/setUserAgent.html';
 const browserScope = new BrowserScope();
@@ -19,7 +19,7 @@ describe('setUserAgent', () => {
 
   it('sets a user agent', async () => {
     await setUserAgent.call(browserScope, 'Setting a user agent');
-    await waitFor.call(browserScope, 1);
+    await checkElementVisible.call(browserScope, '.user-agent', null, '30');
     await checkContainsText.call(browserScope, '.user-agent', null, 'Setting a user agent');
   });
 
