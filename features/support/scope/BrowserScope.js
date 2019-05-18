d51d87ea2ec720d073f53deae6019c890b51066c
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-05-18 10:21:47 -0400

    Override Puppeteer launch options
    Allow launch options for Puppeteer to be
    set in the package.json using a
    "puppeteerOptions" object.

--- features/support/scope/BrowserScope.js
+++ features/support/scope/BrowserScope.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const puppeteerOptions = require('../../../package.json').puppeteerOptions;
 
 /**
  * Manage the Puppeteer browser and page objects.
@@ -11,14 +12,15 @@ class BrowserScope {
     }
 
     async init(){
+        const defaultOptions = {
+            args: ['--no-sandbox', '--disable-dev-shm-usage'], 
+            ignoreHTTPSErrors: true
+        }
         this.close();
 
         this.page = null;
         this.config = {};
-        this.browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-dev-shm-usage'], 
-            ignoreHTTPSErrors: true
-        });
+        this.browser = await puppeteer.launch({...defaultOptions, ...puppeteerOptions});
     }
 
     async close(){
