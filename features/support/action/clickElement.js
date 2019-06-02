fc0565fb9809f46658571c8a47dfb3e5ba32f35d
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-06-02 10:44:35 -0400

    Change click action to use JS click()
    Puppeteer click is behaving inconsistently and
    page.click(selector) actions are not working
    in all cases.

--- features/support/action/clickElement.js
+++ features/support/action/clickElement.js
@@ -1,5 +1,6 @@
 /**
- * Clicks on an item.
+ * Clicks on an item.  Uses the DOM click() method since Puppeteer `page.click(selector)` behaves
+ * inconsistently.
  * @param {String} selector CSS selector of the item to click.
  * @param {String} waitForSelector If not null, the selector that should exist after the click.
  * test should allow to complete.
@@ -7,13 +8,15 @@
 module.exports = async function(selector, waitForSelector) {
     // Wait until the given selector exists
     if(waitForSelector){
+        /* istanbul ignore next */
         await Promise.all([
             this.page.waitForSelector(waitForSelector),
-            this.page.click(selector)
+            this.page.$eval(selector, e => e.click())
         ]);
 
     // Nothing to wait for, just click
-    } else {               
-        await this.page.click(selector);
+    } else {
+        /* istanbul ignore next */
+        await this.page.$eval(selector, e => e.click());
     }
 };
