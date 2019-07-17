cf1b5c6e52591b8533eb98ffc161dfaa4ca6e9bc
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-07-17 10:03:22 -0400

    Add ability to start/stop Puppeteer JS/CSS coverage

--- test/scope/FeatureScope.test.js
+++ test/scope/FeatureScope.test.js
@@ -11,7 +11,8 @@ describe('FeatureScope', () => {
   it('starts out uninitialized', async () => {    
     featureScope = new FeatureScope();
     expect(featureScope.browserScope).toBe(null);
-    expect(featureScope.feature).toBe(null);
+    expect(featureScope.coverageJs).toBe(null);
+    expect(featureScope.coverageCss).toBe(null);
   });
 
   it('can be initialized', async () => {
@@ -21,6 +22,8 @@ describe('FeatureScope', () => {
     expect(featureScope.feature).toBe('mmmuffins');
     expect(featureScope.browserScope).not.toBe(null);
     expect(featureScope.browserScope.worldParameters).toEqual({});
+    expect(featureScope.coverageJs).toBe(null);
+    expect(featureScope.coverageCss).toBe(null);    
   });
   
   it('can close an existing browser scope object', async () => {
@@ -38,5 +41,27 @@ describe('FeatureScope', () => {
     expect(featureScope.isNewFeature('mmmuffins')).toBe(false);
     expect(featureScope.isNewFeature('pants')).toBe(true);
   });
+
+  it('does not capture JS/CSS coverage when BrowserScope is not initialized', async () => { 
+    await featureScope.browserScope.close();
+    expect(featureScope.coverageJs).toBe(null);
+    expect(featureScope.coverageCss).toBe(null); 
+
+    await featureScope.coverageStart();
+    await featureScope.coverageStop();
+    expect(featureScope.coverageJs).toBe(null);
+    expect(featureScope.coverageCss).toBe(null);  
+  });  
+
+  it('can start and stop JS/CSS coverage reporting when BrowserScope is initialized', async () => { 
+    await featureScope.init();
+    expect(featureScope.coverageJs).toBe(null);
+    expect(featureScope.coverageCss).toBe(null);
+
+    await featureScope.coverageStart();
+    await featureScope.coverageStop();
+    expect(featureScope.coverageJs).toEqual([]);
+    expect(featureScope.coverageCss).toEqual([]);    
+  });  
 
 }); 
